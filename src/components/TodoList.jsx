9becc56b315b9007115f7ff19500592e1fc1e851
Gulshan2222 <gulshiniho@gmail.com>
2024-10-29 18:29:09 +0530

    Initial commit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,25 @@
+import TodoCard from "./TodoCard"
+
+export default function TodoList(props) {
+    const {todos, selectedTab} = props
+
+    const filterTodosList = selectedTab === 'All' ? 
+    todos : 
+    selectedTab === 'Completed' ? 
+        todos.filter(val => val.complete) :
+    todos.filter(val => !val.complete)
+
+  return (
+    <>
+        {filterTodosList.map((todo, todoIndex) => {
+            return(
+                <TodoCard 
+                key={todoIndex} 
+                todo={todo} 
+                {...props} 
+                todoIndex={todos.findIndex(val => val.input == todo.input)}/>
+            )
+        })}
+    </>
+  )
+}
